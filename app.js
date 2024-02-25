e3563781ec3b224bc5ad5e4f13c091d5a9a66781
mmk <mmkkarbalaei@gmail.com>
2024-02-25 23:34:35 +0330

    update vowels

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ button.addEventListener('click', () => {
         const val = textArea.value;
         let count = 0;
         for (let i = 0; i < val.length; i++) {
-            if (val[i] === 'a' || val[i] === 'e' || val[i] === 'i' || val[i] === 'o' || val[i] === 'u')
+            if (val[i] === 'a' || val[i] === 'e' || val[i] === 'i' || val[i] === 'o' || val[i] === 'u'|| val[i] === 'A' || val[i] === 'E' || val[i] === 'I' || val[i] === 'O' || val[i] === 'U')
                 count++;
         }
         output.innerHTML = `the Number of Vowels are ${count}`;
