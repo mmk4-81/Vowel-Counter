a2481acd3feae00fadf88632c7d3ab4659f57df4
mmk <mmkkarbalaei@gmail.com>
2024-02-25 23:10:40 +0330

    text area empty

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 const textArea = document.getElementById('textarea');
 const button = document.getElementById('count');
 const output = document.getElementById('output');
+
+button.addEventListener('click',()=>{
+    if(textArea.value === ''){
+        output.innerHTML = 'please enter some text'
+    }
+   
+})
