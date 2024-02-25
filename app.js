3ee4f31daad9a3062488f6c7016aba404c4b2ebd
mmk <mmkkarbalaei@gmail.com>
2024-02-25 23:32:52 +0330

    change bg color

--- app.js
+++ app.js
@@ -2,19 +2,17 @@ const textArea = document.getElementById('textarea');
 const button = document.getElementById('count');
 const output = document.getElementById('output');
 
-button.addEventListener('click',()=>{
-    if(textArea.value === ''){
+button.addEventListener('click', () => {
+    if (textArea.value === '') {
         output.innerHTML = 'please enter some text'
     }
-    else{
+    else {
         const val = textArea.value;
         let count = 0;
-        for(let i=0;i<val.length;i++){
-            if(val[i]=== 'a' || val[i] === 'e' || val[i] === 'i' || val[i] === 'o' || val[i] === 'u')
+        for (let i = 0; i < val.length; i++) {
+            if (val[i] === 'a' || val[i] === 'e' || val[i] === 'i' || val[i] === 'o' || val[i] === 'u')
                 count++;
         }
-
         output.innerHTML = `the Number of Vowels are ${count}`;
-        
     }
 })
