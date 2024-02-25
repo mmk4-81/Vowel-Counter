66fed9c793456c0487a051e16f452bd99920ae8b
mmk <mmkkarbalaei@gmail.com>
2024-02-25 23:23:04 +0330

    done js code

--- app.js
+++ app.js
@@ -6,5 +6,15 @@ button.addEventListener('click',()=>{
     if(textArea.value === ''){
         output.innerHTML = 'please enter some text'
     }
-   
+    else{
+        const val = textArea.value;
+        let count = 0;
+        for(let i=0;i<val.length;i++){
+            if(val[i]=== 'a' || val[i] === 'e' || val[i] === 'i' || val[i] === 'o' || val[i] === 'u')
+                count++;
+        }
+
+        output.innerHTML = `the Number of Vowels are ${count}`;
+        
+    }
 })
